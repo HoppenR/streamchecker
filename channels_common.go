c7394e5c15d8c94debd394a5e9e4bb73b80d9d7f
Christoffer Lundell <christofferlundell@protonmail.com>
2025-06-08 22:20:37 +0200

    add interface back

--- channels_common.go
+++ channels_common.go
@@ -4,6 +4,12 @@ import (
 	"time"
 )
 
+type StreamData interface {
+	GetName() string
+	GetService() string
+	IsFollowed() bool
+}
+
 type Streams struct {
 	Strims          *StrimsStreams
 	Twitch          *TwitchStreams
